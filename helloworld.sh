e1b994992347ff7ad264a39a92a1521ac1aa5319
MGraceLed <mgraceled@gmail.com>
2022-07-27 06:06:57 -0700

    ops challenge 02 to great the world

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: MaryGrace Ledesma	                       
+# Date of latest revision: 07/26/2022      
+# Purpose: Print a string to the terminal
+
+# Main
+"I am working perfectly"
+# End
