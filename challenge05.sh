a2d1fd923b0500ff1b1075f2f95782d168bd61cb
MGraceLed <mgraceled@gmail.com>
2022-07-29 21:36:44 -0700

    submitting ops challenge 05

--- challenge05.sh
+++ challenge05.sh
@@ -3,12 +3,22 @@
 # displays running processes, 
 ps aux
 
+answer="yes" 
 
-# asks the user for a PID, 
-echo "this is the end of the search, please enter a pid to kill"
-read pidvar
+while [ $answer == "yes" ]
+do 
+    # asks the user for a PID, 
+    echo "this is the end of the search, please enter a pid to kill"
+    read pidvar
+
+    sudo kill -9 $pidvar
+    # Use a loop in your script, so that it can prompt the user to choose an option.
+    echo "Do you want to continue? (yes/no)" 
+    read answer
+ 
+done
 
 # then kills the process with that PID.
-sudo kill -9 $pidvar
+# sudo kill -9 $pidvar
 # echo $pidvar
 
