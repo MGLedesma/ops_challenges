fc204930b29f00b49281a7b28c4d9355f1c88203
MGraceLed <mgraceled@gmail.com>
2022-08-02 23:43:44 -0700

    submitting ops challenge 07

--- challenge06.sh
+++ challenge06.sh
@@ -5,7 +5,7 @@ file_array=(file6 dir6)
 
 # Create a script that detects if a file or directory exist.
 [ -d ${file_array[0]} ]
-[ -f ${file_arrya[1]} ]
+[ -e ${file_arrya[1]} ]
 
 # then creates it if it does not exist.
 
---
+++ challenge07.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# use lshw command
+lshw
