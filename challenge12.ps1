6b1710e9719a573a3b5e743f4833a395edfca4c7
MGraceLed <mgraceled@gmail.com>
2022-08-10 02:38:50 -0700

    submitting ops challenge 12

--- challenge12.ps1
+++ challenge12.ps1
@@ -1,4 +1,15 @@
-# Script Name: Demo Ops 201 Class 12
-# Author: David Lee
-# Date of last revision:
-# Purpose: Demo tools used in today's Ops Challenge
+# Script Name: Ops Challenge 12 - IP Analysis
+# Author: Marygrace Ledesma
+# Date of last revision: 08/09/2022
+# Purpose: Powershell script that returns the IPv4 address string and no other extraneous information
+
+function Search-IPv4
+{
+    $Search_IPv4 = ipconfig /all > "network_report.txt"
+    Select-String -Path "network_report.txt" -Pattern "IPv4 Address"
+}
+
+Search-Ipv4
+Remove-Item -Path "network_report.txt"
+
+# End
