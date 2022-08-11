e5948f0dd8025274848f30714795647d5cd90ab5
MGraceLed <mgraceled@gmail.com>
2022-08-11 14:46:04 -0700

    resubmit challenge 06

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - [Ops Challenge 02](challenge02.sh)
 - [Ops Challenge 05](challenge05.sh)
 - [Ops Challenge 12](challenge12.ps1)
+- [Ops Challenge 13](challenge13.sh)
--- challenge06.sh
+++ challenge06.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# our script must use at least one array, one loop, and one conditional.
-file_array=(file6 dir6)
-
+# script must use at least one array, one loop, and one conditional.
 # Create a script that detects if a file or directory exist.
-[ -d ${file_array[0]} ]
-[ -e ${file_arrya[1]} ]
-
 # then creates it if it does not exist.
+
+test6=(file6.txt dir6)
+
+if [[ -e ${test6[0]} && -d ${test6[1]} ]]; then 
+    echo "${test6[0]} already exist"
+    echo "${test6[1]} already exist"
+else    
+    touch file6.txt; echo "file6 is created"
+    mkdir dir6; echo "dir6 is created"
+fi
+
 
