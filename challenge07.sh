ed7bd5ad4143eab27475fc7b670b422e7b58933c
MGraceLed <mgraceled@gmail.com>
2022-08-13 03:37:41 -0700

    submit ops challenge 07

--- challenge07.sh
+++ challenge07.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-# use lshw command
-lshw
+# Script: Ops 201 Class 07 Ops Challenge                   
+# Author: MaryGrace Ledesma
+# Date of latest revision: 8/12/2022
+# Purpose: Create a script that display specific system information
+
+# Use lshw to display system information and grep to remove irrelevant information
+# use dmidecode to display BIOS information
+# Add text to indicate which component the script is returning information about
+# run as sudo
+
+echo "--- CPU ---"
+lshw -c processor | grep -A2 Intel
+lshw -c processor | grep -e width
+
+echo "--- RAM ---"
+lshw -c memory | grep -e description -e physical -e size
+
+echo "--- Display Adapter ---"
+lshw -c display | grep -v version | grep -v logical | grep -v display 
+
+echo "--- Network Adapter ---"
+lshw -c network | grep -v network
+
+echo "--- BIOS ---"
+dmidecode -t 0
+
+# End
+
