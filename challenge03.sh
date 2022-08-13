0c27c5a78de4cff9a95f6a610e072fee9eb7a072
MGraceLed <mgraceled@gmail.com>
2022-08-13 03:44:12 -0700

    submitting ops challenge 03

--- challenge03.sh
+++ challenge03.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Script:			Ops 201 Class 03 Ops Challenge Solution
+# Author: 			MaryGrace Ledesma
+# Date of latest revision:	8/13/2022
+# Purpose:			Print a string to the terminal
+
 # write scripts that print login users
+# must have one variable and functions
+
 login=`last`
 
 showlogin () {
@@ -9,4 +16,4 @@ showlogin () {
 
 showlogin
 
-# must have one variable and functions
+# End
